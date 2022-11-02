2a062f907ec5d50fffb589bf441cec50652b0861
Pavel Agafonov <pavel.agafonov.al@gmail.com>
2022-11-02 19:21:14 +0300

    required dependencies and startup

--- dfs/data-node/src/main.rs
+++ dfs/data-node/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use axum::Router;
+use clap::Parser;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Port
+    #[arg(short, long, default_value_t = 40000)]
+    port: u16,
+
+    /// Volume of disk space to use in KB. If not set service will use all disk space
+    #[arg(short, long)]
+    disk_usage: Option<u64>,
+}
+
+#[tokio::main]
+async fn main() {
+    let args: Args = Args::parse();
+
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    axum::Server::bind(&SocketAddr::new(
+        IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
+        args.port,
+    ))
+    .serve(app.into_make_service())
+    .await
+    .unwrap()
 }
