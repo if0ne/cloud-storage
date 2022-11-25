74c15a2ed71683146c21b087f046f18d7fca68bd
Pavel Agafonov <pavel.agafonov.al@gmail.com>
2022-11-25 13:01:16 +0300

    specified client api

---
+++ dfs/cloud-api/src/client.rs
@@ -0,0 +1,39 @@
+use async_trait::async_trait;
+use std::ops::Range;
+use tokio::io::{AsyncReadExt, AsyncWrite};
+
+#[async_trait]
+pub trait CloudClient {
+    async fn make_dir(&self, path: impl AsRef<std::path::Path>);
+
+    async fn create_small_file(&self, path: impl AsRef<std::path::Path>, data: impl AsRef<[u8]>);
+    async fn create_small_file_from_stream(
+        &self,
+        path: impl AsRef<std::path::Path>,
+        stream: impl AsyncReadExt + AsyncWrite,
+    );
+
+    async fn create_large_file(&self, path: impl AsRef<std::path::Path>, data: impl AsRef<[u8]>);
+    async fn create_large_file_from_stream(
+        &self,
+        path: impl AsRef<std::path::Path>,
+        stream: impl AsyncReadExt + AsyncWrite,
+    );
+
+    async fn read_small_file(
+        &self,
+        path: impl AsRef<std::path::Path>,
+    ) -> impl AsyncReadExt + AsyncWrite;
+    async fn read_large_file(
+        &self,
+        path: impl AsRef<std::path::Path>,
+        blocks: Range<usize>,
+    ) -> impl AsyncReadExt + AsyncWrite;
+
+    async fn commit_to_small_file(&self, path: impl AsRef<std::path::Path>, data: impl AsRef<[u8]>);
+    async fn commit_to_small_file_from_stream(
+        &self,
+        path: impl AsRef<std::path::Path>,
+        stream: impl AsyncReadExt + AsyncWrite,
+    );
+}
